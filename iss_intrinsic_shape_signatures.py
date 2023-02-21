8d9add6eb3f0f4e2a5f470d285b630089281236c
Tengfei Da <tengfeida@autox.ai>
2023-02-21 15:21:54 +0800

    Complete ISS

--- iss_intrinsic_shape_signatures.py
+++ iss_intrinsic_shape_signatures.py
@@ -1,5 +1,6 @@
 import numpy as np
 import open3d as o3d
+import time
 
 
 def read_off(filename):
@@ -7,7 +8,7 @@ def read_off(filename):
     faces = []
     with open(filename, 'r') as f:
         first = f.readline()
-        if (len(first) > 4): 
+        if (len(first) > 4):
             n, m, c = first[3:].split(' ')[:]
         else:
             n, m, c = f.readline().rstrip().split(' ')[:]
@@ -24,60 +25,191 @@ def read_off(filename):
     return points, faces
 
 
-def draw_points_cloud(points, indices):
-    # input: points, np.array of [n,3]
-
+def draw_visualize(point_cloud, line_pcd=None, sphere_pcds=None):
     vis = o3d.visualization.Visualizer()
     vis.create_window()
 
     render_option = vis.get_render_option()
-    render_option.background_color=np.array([0,0,0]) #设置背景为黑色
-    render_option.point_size = 2.0 #设置点云显示尺寸，尺寸越大，点显示效果越粗
-    render_option.show_coordinate_frame = True #显示坐标系
+    render_option.background_color = np.array([0, 0, 0])  # 设置背景为黑色
+    render_option.point_size = 2.0  # 设置点云显示尺寸，尺寸越大，点显示效果越粗
+    render_option.show_coordinate_frame = True  # 显示坐标系
+
+    vis.add_geometry(point_cloud)
+    if line_pcd:
+        vis.add_geometry(line_pcd)
+    if sphere_pcds:
+        for sphere_pcd in sphere_pcds:
+            vis.add_geometry(sphere_pcd)
+
+    vis.run()  # 显示窗口，会阻塞当前线程，直到窗口关闭
+    vis.destroy_window()  # 销毁窗口，该函数必须从主线程调用
+
+
+def get_points_cloud_pcd(points, all_indices):
+    # input: points, np.array of [n,3]
 
     point_cloud = o3d.geometry.PointCloud()
     point_cloud.points = o3d.utility.Vector3dVector(points)
     # point_cloud.paint_uniform_color([1,1,1])
 
-    colors=np.ones((points.shape[0], 3))
-    colors[indices] = np.asarray([1,0,0])
+    colors = np.ones((points.shape[0], 3))
+    for indices in all_indices:
+        colors[indices] = np.asarray([1, 0, 0])
     point_cloud.colors = o3d.utility.Vector3dVector(colors)
-
-    vis.add_geometry(point_cloud) #添加显示的点云对象
-    # vis.add_geometry(mesh_sphere)
-
-    vis.run() #显示窗口，会阻塞当前线程，直到窗口关闭
-    vis.destroy_window() #销毁窗口，该函数必须从主线程调用
+    return point_cloud
 
 
-def get_neighbor_points(points, point, r):
-    diff = points - point
+def get_eigenvectors_pcd(point, eigenvectors):
+    end1 = point + np.squeeze(np.asarray(eigenvectors[:, 2])*100)
+    end2 = point + np.squeeze(np.asarray(eigenvectors[:, 1])*60)
+    end3 = point + np.squeeze(np.asarray(eigenvectors[:, 0])*30)
+    line_points = np.vstack((point, end1, end2, end3))
+
+    line_pcd = o3d.geometry.LineSet()
+    line_pcd.points = o3d.utility.Vector3dVector(line_points)
+    line_pcd.lines = o3d.utility.Vector2iVector(
+        np.asarray([[0, 1], [0, 2], [0, 3]]))
+    line_pcd.colors = o3d.utility.Vector3dVector(
+        [[1, 0, 0], [0, 1, 0], [0, 0, 1]])
+    return line_pcd
+
+
+def get_sphere_pcd(center, r):
+    mesh_sphere = o3d.geometry.TriangleMesh.create_sphere(radius=r/2)
+    mesh_sphere.compute_vertex_normals()
+    mesh_sphere.paint_uniform_color([1, 0.1, 0.1])
+    vertices = np.asarray(mesh_sphere.vertices) + center
+    mesh_sphere.vertices = o3d.utility.Vector3dVector(vertices)
+    return mesh_sphere
+
+
+def get_neighbor_points(points, query_index, r):
+    diff = points - points[query_index]
     dist = np.linalg.norm(diff, axis=1)
-    indices = dist < r
-    neighbors = points[indices]
-    return neighbors, indices
-    
-    # mesh_sphere = o3d.geometry.TriangleMesh.create_sphere(radius=r/2)
-    # mesh_sphere.compute_vertex_normals()
-    # mesh_sphere.paint_uniform_color([1, 0.1, 0.1])
-    # vertices = np.asarray(mesh_sphere.vertices) + point
-    # mesh_sphere.vertices=o3d.utility.Vector3dVector(vertices)
-    # return neighbors, mesh_sphere
+    indices = dist <= r
+    indices[query_index] = False
+    return indices
 
-def iss(points):
 
-    # 1. 对每个点选取周围r半径内的邻域点
+def get_points_weights(points, r):
+    weights = np.zeros(points.shape[0])
+    for i, point in enumerate(points):
+        diff = points - point
+        dist = np.linalg.norm(diff, axis=1)
+        neighbors_num = (dist <= r).sum()
+        weights[i] = 1 / neighbors_num
+    return weights
 
-    # 2. 对每个邻域内的所有点，一起计算加权PCA
 
-    # 3. 根据PCA得到的特征值判断是否为特征点
+def PCA(points, query):
+    points = points - query
+    # centroid = np.mean(points, axis=0)
+    # points = points - centroid
+    # print("center", center)
+    # print("centroid", centroid)
+    H = np.matmul(points.transpose(), points)
+    eigenvalues, eigenvectors = np.linalg.eig(H)
+    sort_index = eigenvalues.argsort()
+    eigenvalues = eigenvalues[sort_index]
+    eigenvectors = eigenvectors[:, sort_index]
+    print("eigenvalues", eigenvalues)
+    print("eigenvectors\n", eigenvectors)
 
-    # 4. 将特征点特殊显示出来
+    return eigenvalues, eigenvectors
+
+
+def WPCA(points, query_point, weights):
+    points = points - query_point
+    weights_matrix = np.diag(weights)
+    H = np.matmul(np.matmul(points.transpose(), weights_matrix), points)
+    H = H / weights.sum()
+    eigenvalues, _ = np.linalg.eig(H)
+    eigenvalues = np.sort(eigenvalues)
+    return eigenvalues
+
+
+def ISS(points, radius=20):
+    start_time = time.time()
+
+    # 0. 计算每个点的R邻域内点的数量，在后面计算权重用
+    weights = get_points_weights(points, radius)
+    pnum = points.shape[0]
+    alpha = np.zeros(pnum)
+    beta = np.zeros(pnum)
+    min_eigenvalue = np.zeros(pnum)
+    is_feature = np.zeros(pnum, dtype=bool)
+
+    all_indices = []
+    point_pcd = o3d.geometry.PointCloud()
+    point_pcd.points = o3d.utility.Vector3dVector(points)
+    pcd_tree = o3d.geometry.KDTreeFlann(point_pcd)
+
+    end_time = time.time()
+    print("Build KD-Tree: {:.2f}秒".format(end_time - start_time))
+
+    for query_index, query_point in enumerate(points):
+        if query_index % 1000 == 0:
+            print("query_index:", query_index, "/", pnum)
+            if query_index >= 5000:
+                break
+
+        # 1. 对每个点选取周围r半径内的邻域点
+        [_, idx, _] = pcd_tree.search_radius_vector_3d(
+            points[query_index], radius)
+        neighbor_indices = np.asarray(idx)
+        # neighbor_indices = np.delete(
+        #     neighbor_indices, np.where(neighbor_indices == query_index))
+        all_indices.append(neighbor_indices)
+
+        # 2. 对每个邻域内的所有点，一起计算加权PCA
+        eigenvalues = WPCA(points[neighbor_indices],
+                           query_point, weights[neighbor_indices])
+
+        # 3. 记录最小特征值大于0的结果
+        min_eigenvalue[query_index] = eigenvalues[0]
+        if min_eigenvalue[query_index] < 1e-3:
+            continue
+
+        alpha[query_index] = eigenvalues[1] / eigenvalues[0]
+        beta[query_index] = eigenvalues[2] / eigenvalues[1]
+
+        # 4. 根据特征值之间比例关系判断是否为特征点
+        if alpha[query_index] > 5 and beta[query_index] > 5:
+            is_feature[query_index] = True
+            # print("query_index:", query_index)
+            # print("alpha:", alpha[query_index])
+            # print("beta:", beta[query_index])
+            # print()
+
+    # NMS
+    print("Before NMS, features num:", is_feature.sum())
+    for query_index, neighbor_indices in enumerate(all_indices):
+        if is_feature[query_index] == False:
+            continue
+        for index in neighbor_indices:
+            if is_feature[index] == True and min_eigenvalue[index] < min_eigenvalue[query_index]:
+                is_feature[index] = False
+    print("After NMS, features num:", is_feature.sum())
+
+    feature_neighbors_indices = [indices for i, indices in enumerate(
+        all_indices) if is_feature[i] == True]
+    point_cloud_pcd = get_points_cloud_pcd(points, feature_neighbors_indices)
+
+    sphere_pcds = []
+    for i, flag in enumerate(is_feature):
+        if flag == True:
+            sphere_pcd = get_sphere_pcd(points[i], radius)
+            sphere_pcds.append(sphere_pcd)
+    end_time = time.time()
+    print("耗时: {:.2f}秒".format(end_time - start_time))
+
+    draw_visualize(point_cloud_pcd, sphere_pcds=sphere_pcds)
 
     return
 
+
 def main():
-    off_filename = "./data/airplane_0627.off"
+    off_filename = "./data/airplane_0628.off"
     points, _ = read_off(off_filename)
     min_pos = points.min(axis=0)
     max_pos = points.max(axis=0)
@@ -86,10 +218,21 @@ def main():
     print("min_pos: ", min_pos)
     print("max_pos: ", max_pos)
 
-    neighbors, indices = get_neighbor_points(points, points[0], 20)
-    print(neighbors.shape)
+    radius = 0.02 * (max_pos-min_pos).max()
+    print("radius for ISS: ", radius)
+    ISS(points, radius)
 
-    draw_points_cloud(points, indices)
+    # point_cloud = o3d.geometry.PointCloud()
+    # point_cloud.points = o3d.utility.Vector3dVector(points)
+    # pcd_tree = o3d.geometry.KDTreeFlann(point_cloud)
+    # [_, idx, _] = pcd_tree.search_radius_vector_3d(points[0], radius)
+    # neighbor_indices = get_neighbor_points(points, 0, radius)
+    # idx = np.asarray(idx)
+    # idx = np.delete(idx, np.where)
+    # print("kdtree:", idx[0:20])
+    # indices = np.where(neighbor_indices == True)
+    # indices = np.sort(indices[0])
+    # print("raw:", indices[0:20])
 
 
 if __name__ == '__main__':
